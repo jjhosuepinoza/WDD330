c5e24fb984dbca9a81c2c003e35980c2507df85d
jjhosuepinoza <97245422+jjhosuepinoza@users.noreply.github.com>
2022-06-05 01:44:21 +0200

    commit

---
+++ js/main.js
@@ -0,0 +1,88 @@
+
+// const btn = document.getElementById('link')
+// btn.addEventListener('click', function() {
+//     alert('Hello World!');
+// });
+
+
+// const bt = document.getElementById('button');
+
+//  const rainbow = ['red','orange','yellow','green','blue','rebeccapurple','violet'];
+
+// function change() {      
+//     document.body.style.background = rainbow[Math.floor(7*Math.random())];
+// }
+// bt.addEventListener('click', change);
+//quiz
+// View Object
+// View Object
+
+
+
+// questions
+const quiz = [
+    { name: "Superman",realName: "Clark Kent" },
+    { name: "Wonder Woman",realName: "Diana Prince" },
+    { name: "Batman",realName: "Bruce Wayne" },
+];
+
+// View Object
+const view = {
+    start: document.getElementById('start'),
+    score: document.querySelector('#score strong'),
+    question: document.getElementById('question'),
+    result: document.getElementById('result'),
+    info: document.getElementById('info'),
+    render(target,content,attributes) {
+        for(const key in attributes) {
+            target.setAttribute(key, attributes[key]);
+        }
+        target.innerHTML = content;
+    },
+    show(element){
+        element.style.display = 'block';
+    },
+    hide(element){
+        element.style.display = 'none';
+    }
+};
+//game
+const game = {
+    start(quiz){
+        this.questions = [...quiz];
+        this.score = 0;
+        // main game loop
+        for(const question of this.questions){
+        this.question = question;
+        this.ask();
+        }
+        // end of main game loop
+        this.gameOver();
+        view.hide(view.start);
+    },
+    ask(){
+        const question = `What is ${this.question.name}'s real name?`;
+        view.render(view.question,question);
+        const response =  prompt(question);
+        this.check(response);
+    },
+    check(response){
+        const answer = this.question.realName;
+        if(response === answer){
+        view.render(view.result,'Correct!',{'class':'correct'});
+        alert('Correct!');
+        this.score++;
+        view.render(view.score,this.score);
+        } else {
+        view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
+        alert(`Wrong! The correct answer was ${answer}`);
+        }
+    },
+    gameOver(){
+        view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+        view.show(view.start);
+    }
+}
+game.start(quiz);
+
+view.start.addEventListener('click', () => game.start(quiz), false);
